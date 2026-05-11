57ba001543a16f58f4826dd4efd87fd95a065110
Sharhad Bashar <sharhad.bashar@uwaterloo.ca>
2026-05-11 16:06:26 -0700

    updated endpoint

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://workshoppefitness.com", "https://www.workshoppefitness.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
